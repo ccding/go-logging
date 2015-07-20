d45cc74505d886a0e99bc5b6a37baa8b427c15c5
Cong Ding <dinggnu@gmail.com>
2015-07-20 12:02:48 -0700

    change default filereader mask

--- logging/logging.go
+++ logging/logging.go
@@ -98,7 +98,7 @@ func RichLogger(name string) (*Logger, error) {
 
 // FileLogger creates a new logger with file output.
 func FileLogger(name string, level Level, format string, timeFormat string, file string, sync bool) (*Logger, error) {
-	out, err := os.OpenFile(file, os.O_WRONLY|os.O_APPEND|os.O_CREATE, os.ModeAppend|0666)
+	out, err := os.OpenFile(file, os.O_WRONLY|os.O_APPEND|os.O_CREATE, os.ModeAppend|0644)
 	if err != nil {
 		return nil, err
 	}
